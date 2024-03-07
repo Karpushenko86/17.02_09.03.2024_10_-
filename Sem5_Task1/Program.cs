a84589c81c3775c40f3f0f15b270c890d63139de
Danya <saikirisoisor@mail.ru>
2024-03-07 19:02:09 +0500

    Add Sem5:Task1

---
+++ Sem5_Task1/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 1.
+// Задайте двумерный массив. 
+// Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+// Пример
+// 2 3 4 3           4 3 16 3
+// 4 3 4 1     =>    4 3  4 1
+// 2 9 5 4           4 9 25 4
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < rows; i++)  //  Вместо 'rows' можно использоваться 'matrix.GetLength(0)'
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = rnd.Next(min, max);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j], 4}");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ReplaceEvenIdxElemToSquare(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i+=2)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j+=2)
+        {
+            matrix[i, j] *= matrix[i, j];
+        }
+    }
+}
+
+int[,] array2d = CreateMatrixRndInt(3, 4, 0, 10);
+PrintMatrix(array2d);
+Console.WriteLine();
+ReplaceEvenIdxElemToSquare(array2d);
+PrintMatrix(array2d);
