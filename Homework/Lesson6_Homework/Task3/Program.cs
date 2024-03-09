47e3ed73de15f65bfe053a1c705841cf55ebc4fd
Danya <saikirisoisor@mail.ru>
2024-03-09 21:32:22 +0500

    Complete Lesson6_Homework:Task3

---
+++ Homework/Lesson6_Homework/Task3/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 3.
+// Задайте произвольную строку. 
+// Выясните, является ли она палиндромом.
+// Пример
+// “aBcD1ef!-”  => Нет
+// “шалаш”      => Да
+// “55655”      => Да
+
+static string IsMessageString(string? input)                    // Проверка на вхождение пустой строки
+{
+    if (input is not null)
+    {
+        return input;
+    }
+
+    return "";
+}
+
+static char[] StringToCharArray(string str)                     // Перевод строки в массив символов
+{
+    char[] charsArray = new char[str.Length];
+
+    for (int i = 0; i < charsArray.Length; i++)
+    {
+        charsArray[i] = str[i];
+    }
+
+    return charsArray;
+}
+
+static void IsStringPalindrome(string input)                    
+{
+    char[] charsArray = StringToCharArray(input);
+
+    int ArrLength = charsArray.Length;
+
+    int count = 0;
+    for (int i = 0; i < ArrLength / 2; i++)
+    {
+        if (charsArray[i] == charsArray[ArrLength - i - 1])
+        {
+            continue;
+        }
+        else
+        {
+            count++;
+            break;
+        }
+    }
+
+    if (count > 0)
+    {
+        Console.Write($"{input} => Нет");
+    }
+    else
+    {
+        Console.Write($"{input} => Да");
+    }
+}
+
+Console.Write("Введите произвольную строку: ");
+string str = IsMessageString(Console.ReadLine());
+IsStringPalindrome(str);
