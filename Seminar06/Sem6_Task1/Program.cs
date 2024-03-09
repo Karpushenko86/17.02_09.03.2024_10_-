55cfd9d6d497f6294347aa3fc21bdcab3743aa4f
Danya <saikirisoisor@mail.ru>
2024-03-09 16:57:02 +0500

    Add Seminar06:Sem6_Task1

---
+++ Seminar06/Sem6_Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задание 1.
+// Задайте массив символов (тип char []). 
+// Создайте строку из символов этого массива.
+// Указание конструктор строки вида string(char []) не использовать.
+// Пример
+// [‘a’, ‘b’, ‘c’, ‘d’] => “abcd”
+
+string GetStringFormCharArray(char[] chars)
+{
+    string str = string.Empty;      // Присвоение пустой строки [ string.Empty <=равносильно=> "" ]
+
+    for (int i = 0; i < chars.Length; i++)
+    {
+        str += chars[i] + " ";
+    }
+
+    return str;
+}
+
+char[] charsArray = {'a', 'b', 'c', 'd'};
+Console.WriteLine(GetStringFormCharArray(charsArray));
