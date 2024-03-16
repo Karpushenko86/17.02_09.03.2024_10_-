9927dcfe81fe1d4a69fdb9326610c33440f70af4
Danya <saikirisoisor@mail.ru>
2024-03-16 13:21:57 +0500

    Add Seminar07:Sem7_Task3

---
+++ Seminar07/Sem7_Task3/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задание 3. 
+// Считать строку с консоли, содержащую латинские буквы.
+// Вывести на экран согласные буквы этой строки.
+// 
+// Указание
+// Использовать рекурсию. Не использовать цикл.
+// 
+// Пример
+// “hello” => h l l
+// “World” => W r l d
+// “Hello world!” => H l l w r l d
+
+void ShowConsonantsInString(string str)
+{
+    string vowels = "eyuioaj";
+
+    if (str.Length == 0) return;
+
+    if (!vowels.Contains(str[0])) 
+    {
+        Console.Write($"{str[0]} ");
+    }
+
+    ShowConsonantsInString(str.Substring(1));
+}
+
+Console.Write("Введите строку, содержащую латинские буквы: ");
+string str = Console.ReadLine();
+
+Console.Write($"“{str}” => ");
+ShowConsonantsInString(str);
