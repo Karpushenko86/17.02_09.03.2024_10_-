a8dd58069f300a748211b4bac600f1d41db2e793
Danya <saikirisoisor@mail.ru>
2024-03-16 12:49:57 +0500

    Add Seminar07:Sem7_Task1

---
+++ Seminar07/Sem7_Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 1.
+// Задайте значение N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+// 
+// Указание
+// Использовать рекурсию. Не использовать цикл.
+// 
+// Пример
+// N = 5 => 1 2 3 4 5
+
+void NaturalNumbers(int num)
+{
+    if(num == 0) return;
+
+    NaturalNumbers(num - 1);
+
+    Console.Write($"{num} ");
+}
+
+Console.Write("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+NaturalNumbers(number);
+
