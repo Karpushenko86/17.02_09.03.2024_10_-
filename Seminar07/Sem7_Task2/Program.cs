30913172bbe00526fbb1a4735c2a2e09624be322
Danya <saikirisoisor@mail.ru>
2024-03-16 13:01:43 +0500

    Add Seminar07:Sem7_Task2

---
+++ Seminar07/Sem7_Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 2.
+// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 
+// Указание
+// Использовать рекурсию.
+// 
+// Пример
+// 123 => 6
+// 63 => 9
+
+int SumOfNumbers(int number)
+{
+    if (number == 0) return 0;
+
+    return number % 10 + SumOfNumbers(number / 10);
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{number} => {SumOfNumbers(number)}");
